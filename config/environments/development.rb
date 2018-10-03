4e652c65c0a67882892bc636d27672bfbc3fe657
jfrazier <johnfrazier1222@gmail.com>
2018-10-03 13:05:03 -0400

    production work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   enable_starttls_auto: true  }
 
 
-  
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,11 @@ Rails.application.configure do
 
 
 
+
+
+
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
