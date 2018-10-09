b3e77efa61dc2bf6df83aa60fe35e30d273ef1eb
jfrazier <johnfrazier1222@gmail.com>
2018-10-09 11:22:57 -0400

    home nav fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   has_many :comments
   # has_one_attached :image
-  mount_uploader :photo, PhotoUploader
+
 end
--- app/uploaders/photo_uploader.rb
+++
@@ -1,3 +0,0 @@
-class PhotoUploader < CarrierWave::Uploader::Base
-  storage :fog
-end
