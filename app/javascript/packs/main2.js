a4a95b4f4c198b4321b6af99522c07c959831484
jfrazier <johnfrazier1222@gmail.com>
2018-10-11 15:16:52 -0400

    intro fixed

--- app/javascript/packs/main2.js
+++ app/javascript/packs/main2.js
@@ -2,10 +2,10 @@ import Vue from 'vue/dist/vue.esm'
 
 document.addEventListener('DOMContentLoaded', () => {
   const test = new Vue({
-    el: '#vuetest',
+    el: '#intro',
     data: {
-      message: "finally"
+      message: "Welcome to The Whiskey man. Whiskey is a very vast and complex type of liquor and can be difficult to find out about different ones. So we have created this place for people who want to learn about whiskey form whiskey drinkers."
     }
   })
-  
+
 })
